73136d0515643fbf2de345e402bb1a45b72a0b16
ssgandham <gandhamsnehalsrinivas@gmail.com>
2023-09-16 19:04:35 -0400

    Home Screen

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var btStart: Button = findViewById(R.id.btStart)
+        var etText: AppCompatEditText = findViewById(R.id.etText)
+
+        btStart.setOnClickListener{
+            val enteredText = etText.text.toString()
+
+            if(enteredText.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Make sure to enter the name ${enteredText}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }else{
+                val intent = Intent(this, QuestionsActiity::class.java)
+                startActivity(intent)
+            }
+        }
     }
-}
+    }
---
+++ app/src/main/java/com/example/quiz/QuestionsActiity.kt
@@ -0,0 +1,19 @@
+package com.example.quiz
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+
+
+class QuestionsActiity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions_actiity)
+
+
+    }
+}
